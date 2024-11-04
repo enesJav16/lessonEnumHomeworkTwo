2cb978da33990954cf4e19bbb5c2d3ba6edfdf6d
enesJav16 <maratuenes@gmail.com>
2024-11-04 13:47:06 +0600

    c

--- src/Days.java
+++ src/Days.java
@@ -1,2 +1,26 @@
+import java.util.Locale;
+
 public enum Days {
+    MONDAY("Java"),
+    TUESDAY("JS"),
+    WEDNESDAY("English"),
+    THURSDAY("Turkish"),
+    FRIDAY("Python"),
+    SATURDAY("Math"),
+    SUNDAY("History");
+    private String lesson;
+
+    Days(String lesson) {
+        this.lesson = lesson;
+    }
+
+    public String getLesson() {
+        return lesson;
+    }
+
+    public void setLesson(String lesson) {
+        this.lesson = lesson;
+    }
+
+
 }
